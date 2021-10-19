6e47e865f52b19f0ef85c463b7a42df19234b787
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-19 15:34:01 -0300

    added inputArea comp. and pageContainer styles

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,23 @@
+import styled from "styled-components";
+
 import InitialAnimation from "../components/InitialAnimation";
+import InputArea from "../components/InputArea";
 
 function App() {
   return (
-    <main>
+    <PageContiner>
       <InitialAnimation />
-    </main>
+      <InputArea />
+    </PageContiner>
   );
 }
+
+const PageContiner = styled.main`
+  background: ${props => props.theme.colors.veryLightGray};
+  min-height: 100vh;
+  display: grid;
+  place-content: center;
+  grid-template-columns: repeat(2, auto);
+`;
 
 export default App;
