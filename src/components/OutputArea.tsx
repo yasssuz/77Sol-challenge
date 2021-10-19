4d713a46defd5643ce0aaa8f95d9bb39bb91ccd8
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-19 18:34:35 -0300

    file created

---
+++ src/components/OutputArea.tsx
@@ -0,0 +1,10 @@
+import { OutputAreaProps } from "../utils/types";
+
+export default function OutputArea({ data }: OutputAreaProps) {
+  console.log(data);
+  return (
+    <>
+      <h1>works</h1>
+    </>
+  );
+}
