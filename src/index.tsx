1d0f813949325fdceea35b071a9c1ba5914e961e
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-19 15:34:21 -0300

    added theming and global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./pages/App";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    white: "#fff",
+    veryLightGray: "#f7f7f7",
+    lightGray: "#acacac",
+    gray: "#4E4B66",
+    blue: "#063ef9",
+  },
+};
+
+const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body, input, select, label, button {
+    font-family: "Roboto", sans-serif;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
