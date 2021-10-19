bcb1e23d4a4b1d81a8e24b0767554c24682c142d
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-19 18:32:59 -0300

    putted most of the interfaces here

---
+++ src/utils/types.tsx
@@ -0,0 +1,26 @@
+export interface UserDataProps {
+  cep: string;
+  currSpendingAmount: string;
+  material: string;
+}
+
+export interface FetchedDataProps {
+  potencial: string;
+  co2: number;
+  parcelas: [
+    {
+      parcelas: number;
+      valor_minimo: number;
+      valor_maximo: number;
+    }
+  ];
+}
+
+export interface InputAreaProps {
+  dataReceiver: (newData: UserDataProps) => Promise<void>;
+  loading: boolean;
+}
+
+export interface OutputAreaProps {
+  data: FetchedDataProps;
+}
