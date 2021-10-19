8758963210300e7be58138e175fe00154c2ffa9e
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-19 18:33:25 -0300

    created fetch system

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import styled from "styled-components";
+
+import { FetchedDataProps, UserDataProps } from "../utils/types";
 
 import InitialAnimation from "../components/InitialAnimation";
 import InputArea from "../components/InputArea";
+import OutputArea from "../components/OutputArea";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState<FetchedDataProps>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function fetchData(userData: UserDataProps): Promise<void> {
+    setIsLoading(true);
+    const { material, cep, currSpendingAmount } = userData;
+    const res = await fetch(
+      `https://api2.77sol.com.br/busca-cep?estrutura=${material}&valor_conta=${currSpendingAmount}&cep=${cep}`
+    );
+    const data = await res.json();
+    const formattedData: FetchedDataProps = {
+      potencial: data.potencial,
+      co2: data.co2,
+      parcelas: data.parcelamento,
+    };
+
+    setFetchedData(formattedData);
+    setIsLoading(false);
+  }
+
   return (
     <PageContiner>
       <InitialAnimation />
-      <InputArea />
+      <InputArea dataReceiver={fetchData} loading={isLoading} />
+      {fetchedData && <OutputArea data={fetchedData} />}
     </PageContiner>
   );
 }
